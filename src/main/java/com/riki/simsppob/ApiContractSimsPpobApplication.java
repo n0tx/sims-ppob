c8b2b1bdd297ed5f47523911e6344eef83e05382
n0tx <rcandra91@msn.com>
2024-12-09 19:01:22 +0700

    - remove .env - remove EnvLoader.java - set application.properties

--- src/main/java/com/riki/simsppob/ApiContractSimsPpobApplication.java
+++ src/main/java/com/riki/simsppob/ApiContractSimsPpobApplication.java
@@ -1,6 +1,5 @@
 package com.riki.simsppob;
 
-import com.riki.simsppob.util.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiContractSimsPpobApplication {
 
 	public static void main(String[] args) {
-		EnvLoader.loadEnv();
 		SpringApplication.run(ApiContractSimsPpobApplication.class, args);
 	}
 
